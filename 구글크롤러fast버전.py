d1f6446524110e7c1b5e8106353b00944d2080d0
newskyy135 <45006278+newskyy135@users.noreply.github.com>
2021-06-21 20:35:18 +0900

    Update 구글크롤러fast버전.py

--- 구글크롤러fast버전.py
+++ 구글크롤러fast버전.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as soups
 def search_selenium(search_name, search_path, search_limit) :
     search_url = "https://www.google.com/search?q=" + str(search_name) + "&hl=ko&tbm=isch"
     
-    browser = webdriver.Chrome(r"C:\Users\sam56\Desktop\구글크롤러\chromedriver")
+    browser = webdriver.Chrome(r"C:\Users\brass\PycharmProjects\newGoogle\chromedriver")
     browser.get(search_url)
     
     image_count = len(browser.find_elements_by_tag_name("img"))
@@ -13,9 +13,19 @@ def search_selenium(search_name, search_path, search_limit) :
 
     browser.implicitly_wait(2)
 
-    for i in range( search_limit ) :
-        image = browser.find_elements_by_tag_name("img")[i]
-        image.screenshot(search_name + str(i) + ".png")
+    if search_limit <= 450:
+        for i in range( search_limit ) :
+            image = browser.find_elements_by_tag_name("img")[i]
+            image.screenshot(search_name + str(i) + ".png")
+    else:
+        for i in range( 450 ) :
+            image = browser.find_elements_by_tag_name("img")[i]
+            image.screenshot(search_name + str(i) + ".png")
+        browser.find_element_by_xpath('//input[@type="button"]').click()
+        for i in range( search_limit - 450 ) :
+            image = browser.find_elements_by_tag_name("img")[i]
+            image.screenshot(search_name + str(i) + ".png")
+
 
     browser.close()
 
